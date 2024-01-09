105d8b42570d015601b103a1f2d0838a3419b9d7
John Tom <mirian.tomani@gmail.com>
2024-01-09 21:47:40 +0000

    upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Work-Day-Scheduler
+
+A basic web-based calendar application designed for users to schedule events hourly. It operates within a browser environment and employs dynamic updates using HTML and CSS managed through jQuery.
+
+This intuitive tool enables users to input objectives for each hour, dynamically adjusting throughout the day to highlight the current hour via JQuery and Moment.js. Additionally, the day planner incorporates localStorage functionality, empowering users to store and retrieve their tasks locally.
+
+## Key Features:
+
+Current Day Display: The calendar prominently displays the current day when accessed by a user.
+
+Hourly Timeblocks: Standard business hours are presented in a scrollable format for scheduling events.
+
+Color-coded Timeblocks: Each timeblock is color-coded to denote past, present, and future hours, offering a visual cue to the user.
+
+Event Entry: Users can effortlessly input events by clicking on a respective timeblock.
+
+Local Storage Functionality: Events are securely saved in local storage upon clicking the save button within each timeblock.
+
+Persistence of Events: The application retains saved events even after a page refresh, ensuring the continuity of scheduled tasks.
+
+The following animation demonstrates the application functionality:
---
+++ starter/index.js
@@ -0,0 +1,53 @@
+// Display today's day and date
+const currentDate = dayjs();
+const todayDate = currentDate.format('dddd, MMMM D YYYY');
+$("#currentDay").html(todayDate);
+
+
+$(document).ready(function () {
+// Changes the color of each time block based on whether it's in the "past, present, or future" relative to the current time.
+    function hourlyColor() {
+        $('.time-block').each(function() {
+            const blockHour = parseInt(this.id.split("hour")[1]);
+
+            if (blockHour < currentDate.hour()) {
+                $(this).removeClass('present future').addClass('past');
+            } else if (blockHour === currentDate.hour()) {
+                $(this).removeClass('past future').addClass('present');
+            } else {
+                $(this).removeClass('past present').addClass('future');
+            }
+        });
+    }
+   
+    function timeTracker() {
+        var nowTime = currentDate.hour(); // Get current hour
+        // loop over time blocks
+        $(".time-block").each(function () { 
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (timeBlock < nowTime) {
+                $(this).removeClass("present future").addClass("past");
+            } else if (timeBlock === nowTime) {
+                $(this).removeClass("past future").addClass("present");
+            } else {
+                $(this).removeClass("present past").addClass("future");
+            }
+        })
+    }
+
+     // Get item from local storage 
+     $("#hour8 .descr").val(localStorage.getItem("hour8"));
+     $("#hour9 .descr").val(localStorage.getItem("hour9"));
+     $("#hour10 .descr").val(localStorage.getItem("hour10"));
+     $("#hour11 .descr").val(localStorage.getItem("hour11"));
+     $("#hour12 .descr").val(localStorage.getItem("hour12"));
+     $("#hour13 .descr").val(localStorage.getItem("hour13"));
+     $("#hour14 .descr").val(localStorage.getItem("hour14"));
+     $("#hour15 .descr").val(localStorage.getItem("hour15"));
+     $("#hour16 .descr").val(localStorage.getItem("hour16"));
+     $("#hour17 .descr").val(localStorage.getItem("hour17"));
+ 
+     timeTracker();
+     hourlyColor();
+});
